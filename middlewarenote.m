eb47a0b400dc54c977949b5cfa28fc13108bbcd4
tuduan1902 <uuxinhxinh2701@gmail.com>
2022-03-06 19:12:30 +0700

    commit

---
+++ middlewarenote.m
@@ -0,0 +1,21 @@
+# Middleware
+# Ý nghĩa
+- Phần mềm trung gian ( đứng giữa các thành phần trong mô hình phần mềm)
+
+Browser (client)  ======= request =====> Server(Node) 
+
+Browser (client) <======= respone ===== Server(Node)
+
+# Vai trò
+- Giống như "Bác bảo vệ"
+
+Nhà =======> Bác bảo vệ (middleware): Sự kiện(soát vé)
+
+Nhà <======= Sự kiện
+
+1. Soát vé (kiểm soát -> Validation)
+2. Không cho vào
+3. Cho phép vào (Validation passed --> cho vào)
+4. Chỉnh sửa / thay đổi
+
+# Ứng dụng
